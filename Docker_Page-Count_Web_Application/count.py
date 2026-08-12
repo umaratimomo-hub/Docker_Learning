375bb32f55e55a8d17a4637336de176734842a02
Umar Atimomo <umar.atimomo@gmail.com>
2026-08-12 03:02:44 +0300

    add modified demo

--- Docker_Page-Count_Web_Application/count.py
+++ Docker_Page-Count_Web_Application/count.py
@@ -13,11 +13,36 @@ r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 def welcome():
     return render_template("index.html")
 
-# Route 2: Counter
+# Route 2:
+@app.route("/architecture")
+def architecture():
+    return render_template("architecture.html")
+
+# Route 3:
+@app.route("/how-it-works")
+def how_it_works():
+    return render_template("how_it_works.html")
+
+# Route 4:
+@app.route("/technology")
+def technology():
+    return render_template("technology.html")
+
+# Route 5:
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+# Route 6: Counter
 @app.route("/count")
 def count():
     visits = r.incr("visits")
     return render_template("count.html", visits=visits)
+
+# Route 7:
+@app.route("/future")
+def future():
+    return render_template("future.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
